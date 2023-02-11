6354761c5cfcbb7a7b4b5c01aa8077270535133b
Franklin Tan <ft229@cornell.edu>
2023-02-11 01:17:02 -0500

    Added code comments

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import dash
-from dash import dcc
-from dash import html
 from funcs import clean_currency
 import pandas as pd
+import numpy as np
+from dash import dcc, html
+import plotly.express as px
 from dash.dependencies import Output, Input
-import plotly.graph_objects as go
 
 # Load original data
 df = pd.read_csv('data/transactions.csv')
@@ -14,12 +14,10 @@ df = df.loc[:, ~df.columns.str.contains('^Unnamed')]  # Removes Unnamed columns
 df['Amount'] = df['Amount'].apply(clean_currency).astype('float')
 df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%y')
 
-print(df.dtypes)
-
 external_stylesheets = [
     {
         "href": "https://fonts.googleapis.com/css2?"
-        "family=Lato:wght@400;700&display=swap",
+                "family=Lato:wght@400;700&display=swap",
         "rel": "stylesheet",
     },
 ]
@@ -31,12 +29,26 @@ app.title = "Bank Statement Analytics: Understand Your Personal Finances!"
 cat_amount = df.drop(columns=["Description", "Address", "City/State", "Zip Code", "Country", ])  # Remove cols
 cat_amount = cat_amount.groupby(cat_amount['Category'])['Amount'].sum().to_frame().reset_index()
 
-print(cat_amount)
+# Create new df with only two tags:
+new_df = df.drop(columns=["Description", "Address", "City/State", "Zip Code", "Country", ])  # Remove cols
+new_df['Type'] = np.where(df['Category'].isin(
+    ['Car Insurance', 'Car Loan', 'Car Maintenance', 'Electric Bill', 'Gas', 'Gas Bill', 'Groceries',
+     'Health Care', 'Housing', 'Internet Bill']), True, False)
+
+new_df['Type'] = new_df['Type'].replace({True: "Necessities", False: "Non-essentials"})
+
+new_df = new_df.groupby(new_df['Type'])['Amount'].sum().to_frame().reset_index()
+
+colors = {'Necessities': '#17B897', 'Non-essentials': '#ff7f0e'}
+
+fig = px.pie(new_df, values='Amount', names='Type', color='Type',
+             color_discrete_map=colors, title='Expense Breakdown')
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div(
     children=[
+        # HEADER INFORMATION
         html.Div(
             children=[
                 html.P(children="🏦", className="header-emoji"),
@@ -50,34 +62,36 @@ app.layout = html.Div(
             ],
             className="header",
         ),
+        # UPLOAD .CSV OR .XLSX FILE
         html.Div(
             children=[
                 html.Div(children="", className="menu-title"),
-                                dcc.Upload(
-                                    id='upload-data',
-                                    children=html.Div([
-                                        'Upload .csv or .xlsx files'
-                                    ]),
-                                    style={
-                                        'width': '15%',
-                                        'height': '60px',
-                                        'lineHeight': '60px',
-                                        'borderWidth': '1px',
-                                        'borderStyle': 'dashed',
-                                        'borderRadius': '5px',
-                                        'textAlign': 'center',
-                                        'margin': '-140px auto 40px auto',
-                                        'background-color': '#FFFFFF'
-                                    },
-                                    # Allow multiple files to be uploaded
-                                    multiple=True
-                                ),
-                                html.Div(id='output-data-upload'),
+                dcc.Upload(
+                    id='upload-data',
+                    children=html.Div([
+                        'Upload .csv or .xlsx files'
+                    ]),
+                    style={
+                        'width': '15%',
+                        'height': '60px',
+                        'lineHeight': '60px',
+                        'borderWidth': '1px',
+                        'borderStyle': 'dashed',
+                        'borderRadius': '5px',
+                        'textAlign': 'center',
+                        'margin': '-140px auto 40px auto',
+                        'background-color': '#FFFFFF'
+                    },
+                    # Allow multiple files to be uploaded
+                    multiple=True
+                ),
+                html.Div(id='output-data-upload'),
             ],
             className="upload",
         ),
         html.Div(
             children=[
+                # INPUT DROP DOWN FOR TOP RANKED EXPENSES
                 html.Div(
                     children=[
                         html.Div(children="Top-Ranked Expenses", className="menu-title"),
@@ -90,6 +104,7 @@ app.layout = html.Div(
                         ),
                     ]
                 ),
+                # INPUT DROP DOWN FOR BOTTOM RANKED EXPENSES
                 html.Div(
                     children=[
                         html.Div(children="Bottom-Ranked Expenses", className="menu-title"),
@@ -102,6 +117,7 @@ app.layout = html.Div(
                         ),
                     ],
                 ),
+                # INPUT DATE RANGE
                 html.Div(
                     children=[
                         html.Div(
@@ -121,6 +137,7 @@ app.layout = html.Div(
         ),
         html.Div(
             children=[
+                # TIME SERIES CHART
                 html.Div(
                     children=dcc.Graph(
                         id="line-chart",
@@ -128,6 +145,7 @@ app.layout = html.Div(
                     ),
                     className="card",
                 ),
+                # TOP RANKED EXPENSES BAR CHART
                 html.Div(
                     children=dcc.Graph(
                         id="bar-chart-1",
@@ -135,12 +153,24 @@ app.layout = html.Div(
                     ),
                     className="card",
                 ),
+                # BOTTOM RANKED EXPENSES BAR CHART
                 html.Div(
                     children=dcc.Graph(
                         id="bar-chart-2",
                         config={"displayModeBar": False},
                     ),
                     className="card",
+                ),
+                # EXPENSE BREAKDOWN PIE CHART
+                html.Div(
+                    children=[
+                        html.Div(children="", className="menu-title"),
+                        dcc.Graph(
+                            id='pie-chart',
+                            figure=fig,
+                            className="card",
+                        ),
+                    ],
                 )
             ],
             className="wrapper",
@@ -152,6 +182,7 @@ app.layout = html.Div(
 @app.callback(
     [Output("line-chart", "figure"), Output("bar-chart-1", "figure"), Output("bar-chart-2", "figure")],
     [
+
         Input("top_n", "value"),
         Input("bottom_n", "value"),
         Input("date-range", "start_date"),
@@ -164,8 +195,8 @@ def update_charts(top_rankings, bottom_rankings, start_date, end_date):
     cat_amount_bottom_n = cat_amount.sort_values('Amount', ascending=True).head(bottom_rankings)  # Bottom N
 
     mask = (
-        (df['Date'] >= start_date)
-        & (df['Date'] <= end_date)
+            (df['Date'] >= start_date)
+            & (df['Date'] <= end_date)
     )
     dff = df.loc[mask, :]
     line_chart_figure = {
